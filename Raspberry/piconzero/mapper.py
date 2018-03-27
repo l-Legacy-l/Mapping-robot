d16e0fc08aeeb8f0029bc02a85c2794d71e2cc74
Timothee Simon <xorob0@posteo.net>
2018-03-27 11:08:16 +0200

    Utilisation d'un gap min et d'un gap max

--- Raspberry/piconzero/mapper.py
+++ Raspberry/piconzero/mapper.py
@@ -4,9 +4,12 @@ import piconzero as pz
 import time
 
 # Espace entre le robot et le mur
-gap = 5
+gapmin = 5
+gapmax = 10
 
 pz.init( )
 while True:
-    if front < gap && side < gap :
+    if front > gapmax :
         pz.forward(50)
+    elif gapmin < front < gapmax && side > gapmax :
+        pz.spinRight(50)
