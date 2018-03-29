bcdaeab711b4259cf782c08d7611c39541fd5133
Legacy <fabio.cumbo@std.heh.be>
2018-03-29 11:40:23 +0200

    Séparation de l'HTML Cartographie + problème de redirection résolu

--- Site Web/Connexion.php
+++ Site Web/Connexion.php
@@ -5,21 +5,49 @@
         <link rel="stylesheet" href="Style.css"/>
     </head>
     <body>
-        <script src="Scripts.js"></script>
-        <h1>Connexion au Raspberry Pi</h1>
-        <div id="img">
-            <img src="Images/logo.gif" >
+        <div id="header">
+            <a href="Logout.php">Se déconnecter</a>
+
+            </br>
+            <?php
+
+            session_start();
+
+            $_SESSION['login'] = $_POST['login'];
+            $mdp = sha1($_POST['password']);
+            setcookie('pseudo', $_SESSION['login'], time() + 365*24*3600, null, null, false, true);
+           
+            try
+            {           
+                $bdd = new PDO('mysql:host=localhost;dbname=raspberrypi;charset=utf8', 'root', 'rasp789456');
+
+                $req= $bdd ->prepare('SELECT login,password FROM users WHERE login = :login && password = :password');
+                $req->execute(array('login' => $_SESSION['login'], 'password' => $mdp));
+                $donnees = $req->fetch();
+
+                if(!empty($donnees))
+                {
+		    echo 'Vous êtes connecté !';
+                    $_SESSION['password']=$mdp;
+                    echo "<script type='text/javascript'>document.location.replace('Cartographie.php');</script>";
+                ?>
+                
+                
+                <?php
+                }
+
+                else
+                {
+                    session_destroy();
+                    echo "<script type='text/javascript'>document.location.replace('index.php');</script>";                 
+                }
+            }
+
+            catch (Exception $e) 
+            {
+                echo 'Erreur de connexion à la BD '.$e;
+            }
+             ?>
+       
+            
         </div>
-
-        <form action="Cartographie.php" method="POST">
-            <fieldset>
-            <legend>Connexion</legend>
-            Login : <input  type="text" name="login"><br>
-            Mot de passe : <input id="input_password" type="password" name="password"><br>
-            <input id="button" type="submit" value="OK">
-            </fieldset>
-        </form>
-
-    
-    </body>
-</html>
