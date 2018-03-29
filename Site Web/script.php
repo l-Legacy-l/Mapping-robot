850471acfd8eb1a7ebc0cd65e79adbb8443d9f0c
Legacy <fabio.cumbo@std.heh.be>
2018-03-29 11:42:02 +0200

    Redirection vers la page Cartographie.php

--- Site Web/script.php
+++ Site Web/script.php
@@ -1,23 +1,40 @@
 <?php
 session_start();
 /*
-ini_set('display_errors','on');
+ini_set("display_errors", 1);
+ini_set("track_errors", 1);
+ini_set("html_errors", 1);
 error_reporting(E_ALL);
+*
+exec("/usr/bin/python -c 'print test'", $mavar);
 
-exec('sudo -S python /home/pi/testPython/test.py 2>/dev/null >/dev/null &');
-die("ok");
+
+echo"jepasse";
+
+echo "<script type ='text/javascript'>document.location.replace('Cartographie.php');</script>"
+
  */
-if ($_SESSION['password'])
+if($_SESSION['password'])
 {
-
-	$command = escapeshellcmd('/home/pi/testPython/test.py');
-	$output = shell_exec($command);
-	echo $output;
+$command=escapeshellcmd('/usr/bin/python /home/pi/piconzero/test.py');
+$output=shell_exec($command);
+echo $output;
+/*echo "<script type='text/javascript'>document.location.replace('Cartographie.php');</script>";*/
+header('Location:Cartographie.php');
 }
 
 else
 {
-	echo "<script type='text/javascript'>document.location.replace('index.php');</script>";	
+	echo "<script type='text/javascript'>document.location.replace('index.php');</script>";
+	
 }
 
+/*
+$result=exec("/usr/bin/python /home/testPython/test.py /tmp");
+$result_array=json_decode($result);
+foreach($result_array as $row){
+	echo $row."<br>";i
+*/
+
 ?>
+
