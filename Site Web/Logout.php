efa2e5cc822051494ad3b2a75ed4cccd70fe4b83
Legacy <fabio.cumbo@std.heh.be>
2018-04-03 17:26:24 +0200

    Revert "Revert "page php permettant de lire le contenu de la table data pour pouvoir l'afficher""
    This reverts commit 3ab133ca91276301d8c97fec0c5ce78043eb9698.

--- Site Web/Logout.php
+++ Site Web/Logout.php
@@ -1,5 +1,5 @@
 <?php
 session_start();
 session_destroy();
-echo "<script type='text/javascript'>document.location.replace('Connexion.php');</script>";
+echo "<script type='text/javascript'>document.location.replace('index.php');</script>";
 ?>
